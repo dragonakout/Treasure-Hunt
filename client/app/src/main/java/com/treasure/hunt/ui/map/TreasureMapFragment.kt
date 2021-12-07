f7d8b98f3eb16de7964237fd99b188bf5bfa0638
Maigan Periot <maigan.periot@yahoo.ca>
2021-12-06 23:31:44 -0500

    working map fragment

---
+++ client/app/src/main/java/com/treasure/hunt/ui/map/TreasureMapFragment.kt
@@ -0,0 +1,80 @@
+package com.treasure.hunt.ui.map
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MapStyleOptions
+import com.treasure.hunt.MainActivity
+import com.treasure.hunt.R
+import com.treasure.hunt.databinding.FragmentMapBinding
+import com.treasure.hunt.databinding.FragmentQuestsBinding
+
+class TreasureMapFragment : Fragment(), OnMapReadyCallback {
+
+    private var binding: FragmentMapBinding? = null
+    private var mapView: MapView? = null
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentMapBinding.inflate(inflater, container, false)
+
+        mapView = binding?.root?.findViewById(R.id.map_view)
+        mapView?.onCreate(savedInstanceState)
+        mapView?.getMapAsync(this)
+
+        return binding!!.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+        mapView = null
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        val coords = LatLng(45.38206, -71.92831) // UdeS
+        map.setMinZoomPreference(15.0f)
+        map.setMaxZoomPreference(18.0f)
+        map.setMapStyle(MapStyleOptions.loadRawResourceStyle(activity as Context, R.raw.map_style))
+
+        map.moveCamera(CameraUpdateFactory.newLatLng(coords))
+
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView?.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView?.onResume()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView?.onStop()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView?.onLowMemory()
+    }
+}
